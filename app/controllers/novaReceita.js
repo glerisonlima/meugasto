6d1a519776e9e06d5a621e251eedff9f0b497773
glerison <glerisonlima@gmail.com>
2016-11-24 23:31:03 -0300

    enviando alterações

--- app/controllers/novaReceita.js
+++ app/controllers/novaReceita.js
@@ -1,3 +1,9 @@
 module.exports.index = function(app,req,res){
-  res.render('novaReceita');
+  
+  var connection = app.config.dbConnection();
+  var receitaModel = new app.app.models.receitaModel(connection);
+
+  receitaModel.getTipoReceita(function(error, result){
+    res.render('novaReceita', {validacao:{}, tipoReceitas: result});
+  });
 }
